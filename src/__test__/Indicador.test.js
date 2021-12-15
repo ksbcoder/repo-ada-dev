39c2cdcdb5b3fbbc0e27b5cd8011418c6145c3cd
Dartrix <daniel.ftg@hotmail.com>
2021-12-15 11:49:06 -0500

    Test para el inidicador de seguridad de contraseña

---
+++ src/__test__/Indicador.test.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { render, screen, cleanup } from "@testing-library/react";
+import Indicador from "components/Indicador";
+
+afterEach(cleanup);
+
+it("Renderiza el coomponente", ()=>{
+    render(
+        <Indicador password="12345"/>
+    );
+    expect(screen.getByTestId('Progress-bar')).toBeInTheDocument();
+    expect(screen.getByTestId('Color-label')).toBeInTheDocument();
+});
+it("Cuando no haya niguna contraseña el texto debe ser 'Muy débil' y el color debe ser '#828282'", ()=>{
+    render(
+        <Indicador password=""/>
+    );
+    expect(screen.getByTestId('Color-label')).toHaveTextContent('Muy débil');
+    expect(screen.getByTestId('Color-label')).toHaveStyle({color:"#828282"})
+});
+it("Cuando haya una contraseña segura el texto debe ser diferente de 'Muy débil' y el color diferente de '#828282'", ()=>{
+    render(
+        <Indicador password="R+cV124{"/>
+    );
+    expect(screen.getByTestId('Color-label')).not.toHaveTextContent('Muy débil');
+    expect(screen.getByTestId('Color-label')).not.toHaveStyle({color:"#828282"})
+});
