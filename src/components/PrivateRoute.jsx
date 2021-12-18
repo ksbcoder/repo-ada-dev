480636e042fc73428b3479b84fd46fe104cbca42
Kevin <baquerochavarro@gmail.com>
2021-12-18 13:25:41 -0500

    agregar prueba unitaria par inscripciones

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,14 +1,18 @@
-import { useUser } from 'context/userContext';
-import React from 'react';
+import { useUser } from "context/userContext";
+import React from "react";
 
 const PrivateRoute = ({ roleList, children }) => {
   const { userData } = useUser();
 
   if (roleList.includes(userData.rol)) {
-    return children;
+    return <div data-testid="authorized">children</div>;
   }
 
-  return <div className='text-9xl text-red-500 '>No estás autorizado para ver este sitio.</div>;
+  return (
+    <div className="text-9xl text-red-500 ">
+      No estás autorizado para ver este sitio.
+    </div>
+  );
 };
 
 export default PrivateRoute;
